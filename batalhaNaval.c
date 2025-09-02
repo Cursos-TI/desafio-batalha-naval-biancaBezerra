3013462657b64d6fc0544f525c10b3fb4acc060d
biancaBezerra <bianca.skills@gmail.com>
2025-09-02 13:08:16 -0300

    CONCLUSAO TEMA 4 NIVEL 3

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,38 +1,85 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 #define LINHAS 10
 #define COLUNAS 10
 
-
+// Declaração da função aplicarForma
+void aplicarForma(int forma[5][5], int linha, int coluna, int tabuleiro[LINHAS][COLUNAS]) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (forma[i][j] == 1 && linha + i < LINHAS && coluna + j < COLUNAS) {
+        if (tabuleiro[linha + i][coluna + j] == 3) {
+          tabuleiro[linha + i][coluna + j] = 5; // navio + habilidade
+        } else {
+          tabuleiro[linha + i][coluna + j] = 1; // só habilidade
+        }
+      }
+    }
+  }
+}
 
 int main() {
 
-  srand(time(0));
-
   int tamanhoNavio = 3;
-  int linhaInicialV = rand() % (LINHAS - tamanhoNavio + 1);
-  int colunaInicialV = rand() % COLUNAS;
-  int linhaInicialH = rand() % LINHAS;
-  int colunaInicialH = rand() % (COLUNAS - tamanhoNavio + 1);
-  int linhaInicialD1 = rand() % (LINHAS - tamanhoNavio + 1);
-  int colunaInicialD1 = rand() % (COLUNAS - tamanhoNavio + 1);
-  int linhaInicialD2 = rand() % (LINHAS - tamanhoNavio + 1);
-  int colunaInicialD2 = rand() % (COLUNAS - tamanhoNavio + 1) + (tamanhoNavio - 1);
+  int linhaInicialV = 2;
+  int colunaInicialV = 1;
+  int linhaInicialH = 5;
+  int colunaInicialH = 3;
+  int linhaInicialD1 = 0;
+  int colunaInicialD1 = 0;
+  int linhaInicialD2 = 4;
+  int colunaInicialD2 = 7;
 
-  int tabuleiro[LINHAS][COLUNAS] = {
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0}
-  };
+  // Definindo posições para cada habilidade
+  int coneLinha = 1, coneColuna = 1; // Cone no canto superior esquerdo
+  int cruzLinha = 3, cruzColuna = 5; // Cruz no meio
+  int octaLinha = 6, octaColuna = 1; // Octaedro no canto inferior esquerdo
+
+  int cone[5][5] = {0};
+  
+  int cruz[5][5] = {0};
+  
+  int octaedro[5][5] = {0};
+
+  int tabuleiro[LINHAS][COLUNAS] = {0};
+
+  //HABILIDADES - CONE/CRUZ/OCTAEDRO
+  printf("\nCONE 🚢\n\n");
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      cone[i][j] = 0;
+      if ((i == 1 && j == 2) || (i == 2 && j >= 1 && j <= 3) || (i == 3)) {
+        cone[i][j] = 1;
+      }
+      printf("%d ", cone[i][j]);
+    }
+    printf("\n");
+  }
+
+  printf("\nCRUZ 🚢\n\n");
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      cruz[i][j] = 0;
+      if ((i >= 0 && j == 2) || (i == 2 && j >=  0 && j <= 5)) {
+        cruz[i][j] = 1;
+      }
+      printf("%d ", cruz[i][j]);
+    }
+    printf("\n");
+  }
+
+  printf("\nOCTAEDRO 🚢\n\n");
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      octaedro[i][j] = 0;
+      if ((i == 1 && j == 2) || (i == 2 && j >=  1 && j <= 3) || (i == 3 && j == 2)) {
+        octaedro[i][j] = 1;
+      }
+      printf("%d ", octaedro[i][j]);
+    }
+    printf("\n");
+  }
+
 
   //POSIÇÃO NAVIO VERTICAL
   if (linhaInicialV + tamanhoNavio <= LINHAS){
@@ -70,8 +117,13 @@ int main() {
     }
   }
 
-
+  // APLICAR HABILIDADES EM POSIÇÕES DIFERENTES
+  aplicarForma(cone, coneLinha, coneColuna, tabuleiro);
+  aplicarForma(cruz, cruzLinha, cruzColuna, tabuleiro);
+  aplicarForma(octaedro, octaLinha, octaColuna, tabuleiro);
+  
   printf("\nJOGO BATALHA NAVAL 🚢\n\n");
+  printf("Legenda: 0 = Vazio, 1 = Habilidade, 3 = Navio, 5 = Navio+Habilidade\n\n");
 
   for (int i = 0; i < LINHAS; i++){
     for (int j = 0; j < COLUNAS; j++){
@@ -82,46 +134,13 @@ int main() {
 
 
   printf("\nCOORDENADAS DOS NAVIOS 🚢\n\n");
-
   for (int i = 0; i < LINHAS; i++){
     for (int j = 0; j < COLUNAS; j++){
-      if (tabuleiro[i][j] == 3){
+      if ((tabuleiro[i][j] == 3) || (tabuleiro[i][j] == 5)){
         printf("(%d, %d)\n", i, j );
       }
     }
   }
-
-    
-
-    
-
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
